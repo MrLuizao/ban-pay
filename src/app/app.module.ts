cba282487c539f6df3705fdc0500428d1b56ee5a
Luis VR <lvazquezr@novasolutionsystems.com>
2020-07-18 14:10:50 -0500

    creacion y arquitectura del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { ProductsFormComponent } from './components/products-form/products-form.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ProductsFormComponent,
+    ProductsComponent,
+    ProductsListComponent
   ],
   imports: [
     BrowserModule,
